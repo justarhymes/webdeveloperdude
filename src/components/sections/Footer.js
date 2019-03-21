4d1c36d15479bc7b7de09eeff22eaeced3aa44c4
Justin <justin@dapshow.com>
2019-03-21 10:55:45 -0700

    new angel list link in footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return(
     <footer className="site-footer">
       <div className="grid-x grid-margin-y grid-margin-x">
-        <p className="cell">Find me on <a href="https://github.com/justarhymes" target="social">Github</a>, <a href="http://stackexchange.com/users/2619791/justin-king" target="social">StackExchange</a>, and <a href="https://angel.co/justin-king-2" target="social">AngelList</a>.</p>
+        <p className="cell">Find me on <a href="https://github.com/justarhymes" target="social">Github</a>, <a href="http://stackexchange.com/users/2619791/justin-king" target="social">StackExchange</a>, and <a href="https://angel.co/justarhymes" target="social">AngelList</a>.</p>
       </div>
     </footer>
   );
